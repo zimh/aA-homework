b47981584714ac32fee9bdd81f26c09e25a7430a
zimh <ziminhang@gmail.com>
2019-07-23 23:54:37 -0700

    ORM homework

---
+++ W5D3/skeleton 2/playwright.rb
@@ -0,0 +1,16 @@
+require 'sqlite3'
+require 'singleton'
+
+class PlayDBConnection < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('plays.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Playwright
+
+end
